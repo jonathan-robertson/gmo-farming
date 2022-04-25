cfb33c6702b1ff70e214320fc648cf029e763796
Jonathan Robertson <jrob356@icloud.com>
2022-04-25 01:31:00 -0500

    begin implementing traits

--- generator/main.go
+++ generator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 type plant struct {
@@ -16,8 +17,6 @@ type variant struct {
 	code         rune
 	name         string
 	incompatible []rune
-	singleEffect string
-	doubleEffect string
 }
 
 var plants []plant
@@ -64,15 +63,13 @@ func init() {
 		"Hop",
 		"Pumpkin",
 	}
-
 	for _, name := range names {
 		plants = append(plants, plant{name: "planted" + name, crop: "foodCrop" + name})
 	}
 
 	variants = []variant{
 		{code: 'B', name: "Bonus", incompatible: []rune{'E'}},
-		{code: 'U', name: "Underground", incompatible: []rune{'U', 'S'},
-			singleEffect: `<property name="PlantGrowing.LightLevelGrow" value="0" /><property name="PlantGrowing.LightLevelStay" value="0" />`},
+		{code: 'U', name: "Underground", incompatible: []rune{'U', 'S'}},
 		{code: 'F', name: "Fast"},
 		{code: 'E', name: "Explosive", incompatible: []rune{'B'}},
 		{code: 'R', name: "Renewable", incompatible: []rune{'R'}},
@@ -81,40 +78,8 @@ func init() {
 	}
 }
 
-func printVanillaStages() {
-	fmt.Println("Vanilla Stages")
-	count := 0
-	for _, plant := range plants {
-		for _, stage := range vanillaStages {
-			count++
-			fmt.Printf("|%02d| %s%s\n", count, plant.name, stage)
-		}
-	}
-}
-
-func printVariantStages() {
-	fmt.Println("Variant Stages")
-	count := 0
-	for _, p := range plants {
-		for sNum, s := range variantStages {
-			count++
-			fmt.Printf("|%03d|. %s%s\n", count, p.name, vanillaStages[sNum])
-			for _, v1 := range variants {
-				count++
-				fmt.Printf("  |%03d|. %s%s%s%c\n", count, p.name, s, variantTiers[1], v1.code)
-				for _, v2 := range variants {
-					if v1.isCompatibleWith(v2) {
-						count++
-						fmt.Printf("    |%03d|. %s%s%s%c%c\n", count, p.name, s, variantTiers[2], v1.code, v2.code)
-					}
-				}
-			}
-		}
-	}
-}
-
 // Write all 3 stages to file
-func produceBlock(c chan string, p plant, tier string, gmos ...rune) (err error) {
+func produceBlock(c chan string, p plant, tier int, gmos ...rune) (err error) {
 	for n, stage := range variantStages {
 		var gmoSuffix, gmoTag string
 		switch len(gmos) {
@@ -130,41 +95,77 @@ func produceBlock(c chan string, p plant, tier string, gmos ...rune) (err error)
 		default:
 			return fmt.Errorf("received too many GMOs")
 		}
-		c <- fmt.Sprintf(`        <block name="%s%s%s%s" stage="%s" gmo="%s">`, p.name, stage, tier, gmoSuffix, stage, gmoTag)
-		c <- fmt.Sprintf(`            <property name="Extends" value="%s%s" />`, p.name, vanillaStages[n])
-		if stage == "1" {
+		c <- fmt.Sprintf(`        <block name="%s%sT%d%s" stage="%s" gmo="%s">`, p.name, stage, tier, gmoSuffix, stage, gmoTag)
+		switch stage {
+		case "1":
+			c <- fmt.Sprintf(`            <property name="Extends" value="%s%s" />`, p.name, vanillaStages[n])
 			c <- fmt.Sprintf(`            <property name="CustomIcon" value="%s%s" />`, p.name, vanillaStages[n])
-		}
-		if stage != "3" {
-			c <- fmt.Sprintf(`            <property name="PlantGrowing.Next" value="%s%s%s%s" />`, p.name, variantStages[n+1], tier, gmoSuffix)
-			c <- fmt.Sprintf(`            <drop event="Destroy" name="%s%s%s%s" count="1" />`, p.name, stage, tier, gmoSuffix)
-		}
-		if stage == "3" {
+			c <- fmt.Sprintf(`            <property name="PlantGrowing.Next" value="%s%sT%d%s" />`, p.name, variantStages[n+1], tier, gmoSuffix)
+			c <- fmt.Sprintf(`            <drop event="Destroy" name="%s%sT%d%s" count="1" />`, p.name, stage, tier, gmoSuffix)
+		case "2":
+			c <- fmt.Sprintf(`            <property name="Extends" value="%s%sT%d%s" />`, p.name, stage, tier, gmoSuffix)
+			c <- fmt.Sprintf(`            <property name="PlantGrowing.Next" value="%s%sT%d%s" />`, p.name, variantStages[n+1], tier, gmoSuffix)
+			c <- fmt.Sprintf(`            <drop event="Destroy" name="%s%sT%d%s" count="1" />`, p.name, stage, tier, gmoSuffix)
+		case "3":
+			c <- fmt.Sprintf(`            <property name="Extends" value="%s%s" />`, p.name, vanillaStages[n])
 			c <- fmt.Sprintf(`            <drop event="Harvest" name="%s" count="4" tag="cropHarvest" />`, p.crop)
 			c <- fmt.Sprintf(`            <drop event="Harvest" name="%s" prob="0.5" count="2" tag="bonusCropHarvest" />`, p.crop)
-			c <- fmt.Sprintf(`            <drop event="Destroy" name="%s1%s%s" count="1" prob="0.5" />`, p.name, tier, gmoSuffix)
+			c <- fmt.Sprintf(`            <drop event="Destroy" name="%s1T%d%s" count="1" prob="0.5" />`, p.name, tier, gmoSuffix)
+			if strings.ContainsRune(gmoSuffix, 'R') {
+				c <- fmt.Sprintf(`            <property name="DowngradeBlock" value="%s1T%d%s" />`, p.name, tier, gmoSuffix)
+			}
 		}
 		c <- "        </block>"
 	}
 	return
 }
 
+func produceModifications(c chan string) {
+	// {code: 'U', name: "Underground", incompatible: []rune{'U', 'S'}},
+	c <- `    <append xpath="/blocks/block[contains(@gmo, 'U') and @stage='1']">
+        <property name="PlantGrowing.LightLevelGrow" value="0" />
+        <property name="PlantGrowing.LightLevelStay" value="0" />
+    </append>`
+
+	// {code: 'F', name: "Fast"},
+	c <- `    <append xpath="/blocks/block[contains(@gmo, 'F') and @stage='1' and not @gmo='F,F']">
+        <property name="PlantGrowing.GrowthRate" value="31.5" />
+    </append>`
+	c <- `    <append xpath="/blocks/block[@gmo='F,F' and @stage='1']">
+        <property name="PlantGrowing.GrowthRate" value="15.75" />
+    </append>`
+	/*
+	   	c <- `    <append xpath="">
+	           <property name="" value="" />
+	       </append>`*/
+	/*
+		{code: 'B', name: "Bonus", incompatible: []rune{'E'}},
+		{code: 'E', name: "Explosive", incompatible: []rune{'B'}},
+
+		{code: 'T', name: "Thorny"},
+		{code: 'S', name: "Sweet", incompatible: []rune{'U'}},
+	*/
+}
+
 func produceBlocks(c chan string) {
 	defer close(c)
 	c <- "<config>\n    <append xpath=\"/blocks\">"
 	for _, plant := range plants {
-		for _, tier := range variantTiers {
+		for _, tier := range []int{2, 3} {
 			produceBlock(c, plant, tier)
 			for _, variant1 := range variants {
 				produceBlock(c, plant, tier, variant1.code)
 				for _, variant2 := range variants {
-					produceBlock(c, plant, tier, variant1.code, variant2.code)
+					if variant1.isCompatibleWith(variant2) {
+						produceBlock(c, plant, tier, variant1.code, variant2.code)
+					}
 				}
 			}
 		}
 	}
-
-	c <- "    </append>\n</config>"
+	c <- "    </append>"
+	produceModifications(c)
+	c <- "</config>"
 }
 
 func getFile(filename string) (*os.File, error) {
