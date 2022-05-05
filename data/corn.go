b9a2cef3313b0ed05c20d7409c83e2f798073cc0
Jonathan Robertson <jrob356@icloud.com>
2022-05-05 13:22:35 -0500

    sort properties alphabetically

--- data/corn.go
+++ data/corn.go
@@ -59,81 +59,64 @@ func (corn *Corn) WriteBlockStages(c chan string, traits string) {
 // TODO: <property name="UnlockedBy" value="perkLivingOffTheLand"/>
 func (*Corn) WriteStage1(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedCorn1_%s" stage="1" traits="%s">
-	<property name="Extends" value="cropsGrowingMaster" param1="CustomIcon"/>
-	<property name="CreativeMode" value="Player"/>
-	
-	<property name="Material" value="Mcorn"/> <!-- mostly for the particle effect -->
-	<property name="Shape" value="New"/>
-	<property name="Model" value="corn_sprout_shape"/>
-	<property name="Place" value="Door"/>
-	<property name="PlaceAsRandomRotation" value="true"/>
-	<property name="Mesh" value="cutoutmoveable"/>
-	<property name="MultiBlockDim" value="1,3,1"/>
-	<property name="Texture" value="529"/>
-	<property name="PlantGrowing.Next" value="plantedCorn2_%s"/>
 	<drop event="Destroy" name="plantedCorn1_%s" count="1"/>
-	
+	<property name="CreativeMode" value="Player"/>
 	<property name="CustomIcon" value="plantedCorn1"/>
 	<property name="DescriptionKey" value="plantedCorn1_%sDesc"/>
+	<property name="Extends" value="cropsGrowingMaster" param1="CustomIcon"/>
 	<property name="Group" value="%s"/>
-</block>`,
-		traits,
-		traits,
-		traits,
-		traits,
-		traits,
-		getCraftingGroup(traits))
+	<property name="Material" value="Mcorn"/> <!-- mostly for the particle effect -->
+	<property name="Mesh" value="cutoutmoveable"/>
+	<property name="Model" value="corn_sprout_shape"/>
+	<property name="MultiBlockDim" value="1,3,1"/>
+	<property name="Place" value="Door"/>
+	<property name="PlaceAsRandomRotation" value="true"/>
+	<property name="PlantGrowing.Next" value="plantedCorn2_%s"/>
+	<property name="Shape" value="New"/>
+	<property name="Texture" value="529"/>
+</block>`, traits, traits, traits, traits, getCraftingGroup(traits), traits)
 }
 
 func (*Corn) WriteStage2(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedCorn2_%s" stage="2" traits="%s">
-	<property name="Extends" value="plantedCorn1_%s"/>
-	
-	<property name="Shape" value="New"/>
-	<property name="Model" value="corn_growth_shape"/>
-	<property name="Mesh" value="cutoutmoveable"/>
-	<property name="MultiBlockDim" value="1,3,1"/>
-	<property name="Texture" value="529"/>
-	<property name="PlantGrowing.Next" value="plantedCorn3HarvestPlayer"/>
 	<drop event="Destroy" name="plantedCorn1_%s" count="1"/>
-</block>`,
-		traits,
-		traits,
-		traits,
-		traits)
+	<property name="Extends" value="plantedCorn1_%s"/>
+	<property name="Mesh" value="cutoutmoveable"/>
+	<property name="Model" value="corn_growth_shape"/>
+	<property name="MultiBlockDim" value="1,3,1"/>
+	<property name="PlantGrowing.Next" value="plantedCorn3HarvestPlayer"/>
+	<property name="Shape" value="New"/>
+	<property name="Texture" value="529"/>
+</block>`, traits, traits, traits, traits)
 	// TODO: <property name="CreativeMode" value="None"/>
 }
 
 func (corn *Corn) WriteStage3(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedCorn3_%s" stage="3" traits="%s">
-	<property name="DisplayType" value="blockMulti"/>
-	<property name="LightOpacity" value="0"/>
-	<property name="ImposterDontBlock" value="true"/>
-	<property name="Collide" value="melee"/>
-	<property name="IsTerrainDecoration" value="true"/>
-	<property name="IsDecoration" value="true"/>
-	
-	<property name="PlantGrowing.FertileLevel" value="1"/>
-	<property name="HarvestOverdamage" value="false"/>
 	<drop event="Destroy" count="0"/>
+	<drop event="Destroy" name="plantedCorn1_%s" count="1" prob="0.5"/>
 	<drop event="Fall" name="resourceYuccaFibers" count="0" prob="1" stick_chance="0"/>
-	<property name="FilterTags" value="MC_outdoor,SC_crops"/>
-	<property name="SortOrder1" value="a090"/>
-	<property name="SortOrder2" value="0002"/>
-
-	<property name="Material" value="Mcorn"/>
-	<property name="Shape" value="New"/>
-	<property name="Model" value="corn_harvest_shape"/>
-	<property name="Mesh" value="cutoutmoveable"/>
-	<property name="MultiBlockDim" value="1,3,1"/>
-	<property name="Texture" value="529"/>
-
-	<property name="DescriptionKey" value="plantedCorn3_%s"/>
-	<property name="CustomIcon" value="plantedCorn3Harvest"/>
-
 	<drop event="Harvest" name="foodCropCorn" count="%d" tag="cropHarvest"/>
 	<drop event="Harvest" name="foodCropCorn" prob="0.5" count="%d" tag="bonusCropHarvest"/>
-	<drop event="Destroy" name="plantedCorn1_%s" count="1" prob="0.5"/>
+	<property name="Collide" value="melee"/>
+	<property name="CustomIcon" value="plantedCorn3Harvest"/>
+	<property name="DescriptionKey" value="plantedCorn3_%s"/>
+	<property name="DisplayType" value="blockMulti"/>
+	<property name="FilterTags" value="MC_outdoor,SC_crops"/>
+	<property name="HarvestOverdamage" value="false"/>
+	<property name="ImposterDontBlock" value="true"/>
+	<property name="IsDecoration" value="true"/>
+	<property name="IsTerrainDecoration" value="true"/>
+	<property name="LightOpacity" value="0"/>
+	<property name="Material" value="Mcorn"/>
+	<property name="Mesh" value="cutoutmoveable"/>
+	<property name="Model" value="corn_harvest_shape"/>
+	<property name="MultiBlockDim" value="1,3,1"/>
+	<property name="PlantGrowing.FertileLevel" value="1"/>
+	<property name="Shape" value="New"/>
+	<property name="SortOrder1" value="a090"/>
+	<property name="SortOrder2" value="0002"/>
+	<property name="Texture" value="529"/>
 	%s
 </block>`,
 		traits,
--- data/mushroom.go
+++ data/mushroom.go
@@ -55,76 +55,65 @@ func (mushroom *Mushroom) WriteBlockStages(c chan string, traits string) {
 // TODO: <property name="UnlockedBy" value="perkLivingOffTheLand,plantedMushroom1Schematic"/>
 func (*Mushroom) WriteStage1(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedMushroom1_%s" stage="1" traits="%s">
-	<property name="Extends" value="cropsGrowingMaster" param1="CustomIcon,DescriptionKey,MultiBlockDim,OnlySimpleRotations"/>
-	<property name="CreativeMode" value="Player"/>
-	<property name="DisplayInfo" value="Name"/>
-	<property name="UnlockedBy" value="perkLivingOffTheLand"/>
-	<property name="Material" value="Mmushrooms"/>
-	<property name="LightOpacity" value="0"/>
-	<property name="Shape" value="Ext3dModel"/>
-	<property name="Texture" value="293"/>
-	<property name="Mesh" value="models"/>
-	<property name="Model" value="OutdoorDecor/mushroom_sprout" param1="main_mesh"/>
-	<property name="Collide" value="melee"/>
-	<property name="HandleFace" value="Bottom"/>
-	<property name="PlantGrowing.LightLevelGrow" value="0"/>
-	<property name="PlantGrowing.LightLevelStay" value="0"/>
-	<property name="PlantGrowing.FertileLevel" value="0"/>
-	<property name="PlantGrowing.Next" value="plantedMushroom2_%s"/>
-	<property name="HarvestOverdamage" value="false"/>
 	<drop event="Destroy" name="plantedMushroom1_%s" count="1"/>
-	<property name="EconomicValue" value="12"/>
-	<property name="EconomicBundleSize" value="5"/>
-	<property name="PickupJournalEntry" value="farmingTip"/>
-	
+	<property name="Collide" value="melee"/>
+	<property name="CreativeMode" value="Player"/>
 	<property name="CustomIcon" value="plantedMushroom1"/>
 	<property name="DescriptionKey" value="plantedMushroom1_%sDesc"/>
+	<property name="DisplayInfo" value="Name"/>
+	<property name="EconomicBundleSize" value="5"/>
+	<property name="EconomicValue" value="12"/>
+	<property name="Extends" value="cropsGrowingMaster" param1="CustomIcon,DescriptionKey,MultiBlockDim,OnlySimpleRotations"/>
 	<property name="Group" value="%s"/>
-</block>`,
-		traits,
-		traits,
-		traits,
-		traits,
-		traits,
-		getCraftingGroup(traits))
+	<property name="HandleFace" value="Bottom"/>
+	<property name="HarvestOverdamage" value="false"/>
+	<property name="LightOpacity" value="0"/>
+	<property name="Material" value="Mmushrooms"/>
+	<property name="Mesh" value="models"/>
+	<property name="Model" value="OutdoorDecor/mushroom_sprout" param1="main_mesh"/>
+	<property name="PickupJournalEntry" value="farmingTip"/>
+	<property name="PlantGrowing.FertileLevel" value="0"/>
+	<property name="PlantGrowing.LightLevelGrow" value="0"/>
+	<property name="PlantGrowing.LightLevelStay" value="0"/>
+	<property name="PlantGrowing.Next" value="plantedMushroom2_%s"/>
+	<property name="Shape" value="Ext3dModel"/>
+	<property name="Texture" value="293"/>
+	<property name="UnlockedBy" value="perkLivingOffTheLand"/>
+</block>`, traits, traits, traits, traits, getCraftingGroup(traits), traits)
 }
 
 func (*Mushroom) WriteStage2(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedMushroom2_%s" stage="2" traits="%s">
+	<property name="Collide" value="melee"/>
 	<property name="Extends" value="plantedMushroom1_%s"/>
-	
 	<property name="Model" value="OutdoorDecor/mushroom_growth" param1="main_mesh"/>
 	<property name="PlantGrowing.Next" value="plantedMushroom3_%s"/>
-	<property name="Collide" value="melee"/>
 </block>`, traits, traits, traits, traits)
 	// TODO: <property name="CreativeMode" value="None"/>
 }
 
 func (mushroom *Mushroom) WriteStage3(c chan string, traits string) {
 	c <- fmt.Sprintf(`<block name="plantedMushroom3_%s" stage="3" traits="%s">
-	<property name="Material" value="Mmushrooms"/>
-	<property name="DisplayType" value="blockMulti"/>
-	<property name="Shape" value="Ext3dModel"/>
-	<property name="Texture" value="293"/>
-	<property name="Mesh" value="models"/>
-	<property name="Model" value="OutdoorDecor/mushroom_harvest" param1="main_mesh"/>
-	<property name="Collide" value="melee"/>
-	<property name="VehicleHitScale" value=".1"/>
-	<property name="IsTerrainDecoration" value="true"/>
-	<property name="PlantGrowing.FertileLevel" value="0"/>
-	<property name="CropsGrown.BonusHarvestDivisor" value="16"/>
-	<property name="HarvestOverdamage" value="false"/>
-	
+	<drop event="Destroy" name="plantedMushroom1_%s" count="1" prob="0.5"/>
 	<drop event="Fall" name="resourceYuccaFibers" count="0" prob="1" stick_chance="0"/>
-	<property name="PickupJournalEntry" value="farmingTip"/>
-	<property name="FilterTags" value="MC_outdoor,SC_crops"/>
-
-	<property name="DescriptionKey" value="plantedMushroom3_%s"/>
-	<property name="CustomIcon" value="plantedMushroom3Harvest"/>
-	
 	<drop event="Harvest" name="foodCropMushrooms" count="%d" tag="cropHarvest"/>
 	<drop event="Harvest" name="foodCropMushrooms" prob="0.5" count="%d" tag="bonusCropHarvest"/>
-	<drop event="Destroy" name="plantedMushroom1_%s" count="1" prob="0.5"/>
+	<property name="Collide" value="melee"/>
+	<property name="CropsGrown.BonusHarvestDivisor" value="16"/>
+	<property name="CustomIcon" value="plantedMushroom3Harvest"/>
+	<property name="DescriptionKey" value="plantedMushroom3_%s"/>
+	<property name="DisplayType" value="blockMulti"/>
+	<property name="FilterTags" value="MC_outdoor,SC_crops"/>
+	<property name="HarvestOverdamage" value="false"/>
+	<property name="IsTerrainDecoration" value="true"/>
+	<property name="Material" value="Mmushrooms"/>
+	<property name="Mesh" value="models"/>
+	<property name="Model" value="OutdoorDecor/mushroom_harvest" param1="main_mesh"/>
+	<property name="PickupJournalEntry" value="farmingTip"/>
+	<property name="PlantGrowing.FertileLevel" value="0"/>
+	<property name="Shape" value="Ext3dModel"/>
+	<property name="Texture" value="293"/>
+	<property name="VehicleHitScale" value=".1"/>
 	%s
 </block>`,
 		traits,
