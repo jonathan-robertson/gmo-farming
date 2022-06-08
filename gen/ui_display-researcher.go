37842092fbfe9e48b733932e83ac297edbb8c78e
jonathan-robertson <jrob356@icloud.com>
2022-06-07 20:28:00 -0500

    update hotbox enhancement category icon to upgrade

--- gen/ui_display-researcher.go
+++ gen/ui_display-researcher.go
@@ -19,7 +19,7 @@ func (*ResearcherUIDisplay) Produce(c chan string) {
 	c <- `<config><append xpath="/ui_display_info/crafting_category_display">
     <crafting_category_list display_type="hotbox">
         <crafting_category name="Tier1SeedResearch" icon="ui_game_symbol_book" display_name="lblCategoryTier1SeedResearch" />
-        <crafting_category name="Tier1Seeds" icon="ui_game_symbol_crops" display_name="lblCategoryTier1Seeds" />
+        <crafting_category name="Tier1Seeds" icon="ui_game_symbol_block_upgrade" display_name="lblCategoryTier1Seeds" />
         <crafting_category name="Tier2SeedResearch" icon="ui_game_symbol_book" display_name="lblCategoryTier2SeedResearch" />
         <crafting_category name="Tier2Seeds" icon="ui_game_symbol_add" display_name="lblCategoryTier2Seeds" />
         <crafting_category name="Tier3SeedResearch" icon="ui_game_symbol_book" display_name="lblCategoryTier3SeedResearch" />
--- gen/ui_display-standard.go
+++ gen/ui_display-standard.go
@@ -18,7 +18,7 @@ func (*StandardUIDisplay) Produce(c chan string) {
 	defer close(c)
 	c <- `<config><append xpath="/ui_display_info/crafting_category_display">
     <crafting_category_list display_type="hotbox">
-        <crafting_category name="Tier1Seeds" icon="ui_game_symbol_crops" display_name="lblCategoryTier1Seeds" />
+        <crafting_category name="Tier1Seeds" icon="ui_game_symbol_block_upgrade" display_name="lblCategoryTier1Seeds" />
         <crafting_category name="Tier2Seeds" icon="ui_game_symbol_add" display_name="lblCategoryTier2Seeds" />
         <crafting_category name="Tier3Seeds" icon="ui_game_symbol_healing_factor" display_name="lblCategoryTier3Seeds" />
     </crafting_category_list>
